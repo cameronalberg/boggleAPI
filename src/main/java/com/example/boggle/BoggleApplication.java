d5e7c64ede0ce3204507e887aa05b52eee02c133
Cameron Alberg <cameronalberg@gmail.com>
2022-09-15 11:02:50 -0500

    updated index.html to reference correct css filepath. added spring boot files.

---
+++ src/main/java/com/example/boggle/BoggleApplication.java
@@ -0,0 +1,14 @@
+package com.example.boggle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+
+@SpringBootApplication
+public class BoggleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoggleApplication.class, args);
+    }
+
+}
