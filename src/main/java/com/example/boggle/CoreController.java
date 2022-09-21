8913aa184cbb5309a337ce67db59eed259aafb14
Cameron Alberg <cameronalberg@gmail.com>
2022-09-20 20:31:21 -0500

    updated CORS policy to accept all localhost services. updated /demo endpoint to direct to external URL

--- src/main/java/com/example/boggle/CoreController.java
+++ src/main/java/com/example/boggle/CoreController.java
@@ -2,10 +2,13 @@ package com.example.boggle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
+import java.net.URI;
 import java.sql.SQLException;
 
 @SpringBootApplication
@@ -25,6 +28,12 @@ public class CoreController {
     @RequestMapping(path="/error")
     public String displayError() {
         return "404.html";
+    }
+
+    @RequestMapping(path="/demo")
+    public ResponseEntity<Object> redirect() {
+        return ResponseEntity.status(HttpStatus.PERMANENT_REDIRECT).location(URI.create(
+                "https://boggle-demo.calberg.me")).build();
     }
 
 }
